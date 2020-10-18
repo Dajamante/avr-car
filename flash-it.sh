33aa0c668c58c316b801d4819011d33a4b4ccba2
dajamante <aimaiga2@gmail.com>
2020-10-18 17:06:26 +0200

    changed flash it to do arduino

--- flash-it.sh
+++ flash-it.sh
@@ -1,6 +1,6 @@
 # prevent flashing if error
 set -e cargo +nightly build
 
-avrdude -p atmega328p -c arduino -P /dev/tty.usbserial-14430 -U flash:w:target/avr-atmega328p/debug/avr-robot-car.elf:e
+avrdude -p atmega328p -c arduino -P /dev/tty.usbmodem144201 -U flash:w:target/avr-atmega328p/debug/avr-robot-car.elf:e
 # show on console with baud rate 57600
-screen /dev/tty.usbserial-14430 57600
+screen /dev/tty.usbmodem144201 57600
