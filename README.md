1ed85645d7671690287d96b9aacff440a1846c63
dajamante <aimaiga2@gmail.com>
2020-10-30 20:15:57 +0100

    changes in readme

--- README.md
+++ README.md
@@ -2,47 +2,47 @@
 
 Working with Rahix's avr-hal to make a little robot :car:🐯 with 📡.
 
-11/08/2020:
-Implemented the sensor and the wheels.
-15/08/2020:
-The car is rolling but the board is making a peeping sound and needs to be restarted
-several times on occasions.
-
-The circuit diagram is the same as [this project](https://create.arduino.cc/projecthub/hda-robotics/project-1-2wd-obstacle-avoiding-robot-390ef8).
+The circuit diagram is below:
 
 <div>
-<img src="circuit_diagram.jpg" width="400" />
+<img src="rustyrobot.jpg" width="400" />
   </div>
 
 ## Stuff:
 
-- Arduino UNO or generic using ATmega328p
+- **If you are not used to/have no access to cheap electronic parts**: 🛍🛒 You can also order a kit from Banggood or Ebay. Just search _2WD obstacle avoiding robot kit_, there are many, many, many suppliers. Example: [Ebay UK](https://www.ebay.co.uk/itm/Smart-Car-Motor-Robot-Chassis-Ultrasonic-Module-Battery-Box-Kit-2WD-For-Arduino/174299467397?hash=item28950ece85:g:gy8AAOSwRh9e0P8z), [Banggood](https://www.banggood.com/Geekcreit-DIY-L298N-2WD-Ultrasonic-Smart-Tracking-Moteur-Robot-Car-Kit-for-Arduino-products-that-work-with-official-Arduino-boards-p-1155139.html?rmmds=search&cur_warehouse=CN)...
+- Also, there are great tutorials [on assembling those robots](https://www.youtube.com/watch?v=BH33F-Hi_2M&list=PLN9tittQZUlRJCdE17eORJ7ZGVhGnH77k&index=3)!
+-
+- **If you love picking up electronics yourself**:
+- Arduino UNO or Nano, or any clone using ATmega328p
 - [Servo SG90](https://components101.com/servo-motor-basics-pinout-datasheet)
 - [2 DC motors 12V](http://robotechshop.com/shop/robotics/motors/dc-motors/yellow-gearbox-motor/?v=f78a77f631d2)
 - [Motordriver with H-bridge L298N](https://howtomechatronics.com/tutorials/arduino/arduino-dc-motor-control-tutorial-l298n-pwm-h-bridge/)
 - [Sensor HC-SR04](https://www.amazon.co.uk/dp/B07TKVPPHF/ref=as_li_ss_tl?_encoding=UTF8&psc=1&linkCode=sl1&tag=howtomuk-21&linkId=8faa13eaeab406a33ae606e005699aaf&language=en_GB)
-- cables, jumpers, breadboards...
-- **If you are not used to/have no access to cheap electronic parts**: 🛍🛒 You can also order a kit from Banggood or Ebay. Just search _2WD obstacle avoiding robot kit_, there are many, many, many suppliers. Example: [Ebay UK](https://www.ebay.co.uk/itm/Smart-Car-Motor-Robot-Chassis-Ultrasonic-Module-Battery-Box-Kit-2WD-For-Arduino/174299467397?hash=item28950ece85:g:gy8AAOSwRh9e0P8z), [Banggood](https://www.banggood.com/Geekcreit-DIY-L298N-2WD-Ultrasonic-Smart-Tracking-Moteur-Robot-Car-Kit-for-Arduino-products-that-work-with-official-Arduino-boards-p-1155139.html?rmmds=search&cur_warehouse=CN)... 
-- Also, there are great tutorials [on assembling those robots](https://www.youtube.com/watch?v=BH33F-Hi_2M&list=PLN9tittQZUlRJCdE17eORJ7ZGVhGnH77k&index=3)!
+- cables, jumpers, breadboards.
 
 ## Get started:
 
 1. Install avrdude. It's the utility to write in the ROM(read only memory) or the EEPROM (electrically erasable programmable read-only memory) of AVR microcontrollers. atmega329p, on the arduino uno is from this familly.
 
-2) Modify the executable flash_it.sh. It contains those lines:
+2) Modify the executable flash_it.sh if you want to use it. It contains those lines:
 
 ```
+// build in nightly
 set -e cargo +nightly build
 
+
+// burn on the board
 avrdude -p atmega328p -c arduino -P /dev/tty.usbserial-14430 -U flash:w:target/avr-atmega328p/debug/avr-example.elf:e
 
+// can be removed, used to check the console on mac.
 screen /dev/tty.usbserial-14430 57600
 ```
 
 ### Details:
 
 `set -e` is a bash command that will prevent your board to be flashed if an error is returned by cargo.
-You flash on the board with avrdude with your usb serial and your own elf file. You can get your USB with `s /dev/tty* | grep usb`.
+You flash on the board with avrdude with your usb serial and your own elf file. You can get your USB with `ls /dev/tty* | grep usb`.
 
 - `-p` is "partno": the only mandatory option, tells avrdude what type of MCU is connected to the programmer
 - `-c` gives the programmer id from a list (luckily arduino is super common)
---
+++ src/main_radar.rs
@@ -0,0 +1,103 @@
+//! ## Classical obstacle avoiding robot with Rust
+//! This project takes and Arduino classic and tries to port it to
+//! Rust as a beginner project.
+//! It uses avr-hal crate for abstraction.
+//! For hardware it uses a simple HC-SR04 sensor, an L298N motor driver and a SG90
+//! servo motor (details in the Readme).
+//! The sensor is reading distance every > 100ms and the robot should take appropriate
+//! action if an obstacle is detected.
+
+// Macros to inform rust that the project will not use
+// main and the standard library (lib std adds a layer to build the usual functions.)
+#![no_std]
+#![no_main]
+
+// Pull in the panic handler from panic-halt
+extern crate panic_halt;
+
+use arduino_uno::hal::port::mode::Floating;
+use arduino_uno::prelude::*;
+use crate::sensor::{return_distance, SensorUnit};
+
+mod sensor;
+
+const WAIT_BETWEEN_ACTIONS: u16 = 1000u16;
+const MINIMAL_DISTANCE: u16 = 10u16;
+const ACCEPTABLE_DISTANCE: u16 = 10u16;
+// creates the main function
+// attribute macro -> transforms the next as the entry point
+// "!" is a never type. It informs nothing should return from the main function.
+#[arduino_uno::entry]
+fn main() -> ! {
+    // we acquire first a singleton of all the peripherals (everything inside the MCU)
+    // more information on raw registers abstraction here:
+    // https://docs.rs/avr-device/0.2.1/avr_device/atmega328p/struct.Peripherals.html
+    let dp = arduino_uno::Peripherals::take().unwrap();
+
+    let mut delay = arduino_uno::Delay::new();
+
+    // all the ports are collapsed into the variable pins
+    // docs on all pins: https://rahix.github.io/avr-hal/arduino_uno/struct.Pins.html
+    // by default all pins are configured as Inputs and Floating
+    // (pull up is to avoid undefined state. For arduino boards (5V), pull-up will allow up or down.
+    let mut pins = arduino_uno::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
+
+    // this is the console. To see the output do (on mac)
+    // screen /dev/tty/<your_tty_here> 57600
+    // ls /dev/tty* | grep usb --> get the usb connected
+    // 57600 is the baud rate
+    let mut serial = arduino_uno::Serial::new(
+        // protocol to communicate bytes in 2 directions
+        // USART0 is moved to serial, serial becomes the new owner
+        // https://rahix.github.io/avr-hal/atmega328p_hal/usart/struct.Usart0.html
+        dp.USART0,
+        // the values below correspond to :
+        // rx: receive pin (hardwired into the MCU)
+        // tx : PD1 is the "hardcoded output"
+        // the ownership is moved by writing explicitely input, output is enforced at compile time,
+        pins.d0,
+        pins.d1.into_output(&mut pins.ddr),
+        // other well known baud rates are possible (9600)
+        57600,
+    );
+
+    // initialisation of timer 1 :  we write over and set prescaler to 64
+    // (1/(16e6/64)) * 2^16 (size of register) ~> takes 262 ms for a cycle
+    // timer1 is shared with the sensor unit
+    let timer1 = dp.TC1;
+    timer1.tccr1b.write(|w| w.cs1().prescale_64());
+
+    // servo is best set as a struct for clarity, it will be send to
+
+    let mut sensor_unit = SensorUnit {
+        // We do not use pin 13, because it is also connected to an onboard LED marked "L"
+        // ownership issues: we are moving the pins.d13 into first, the function into_output
+        // second, into led. It needs the ddr register for configuration
+        // (DDRx are used to configure the respective PORT as output/input)
+        trig: pins.d12.into_output(&mut pins.ddr),
+        // floating input is set by default so we can configure echo without ddr
+        echo: pins.d11,
+        timer: timer1,
+    };
+
+
+    'outer: loop {
+
+        let value = return_distance(&mut sensor_unit);
+        ufmt::uwriteln!( & mut serial, "Hello, we are {} cms away from target!\r", value).void_unwrap();
+
+        if value < MINIMAL_DISTANCE {
+                ufmt::uwriteln!( & mut serial, "Help! Give me space!\r").void_unwrap();
+
+                delay.delay_ms(WAIT_BETWEEN_ACTIONS);
+            }
+
+        // the sensor needs to wait approximately 60 ms between two waves.
+        // we ensure that by waiting while the register reaches 25000
+        // one count == 4 us, and 4us*0.000004 == 100 ms
+        while sensor_unit.timer.tcnt1.read().bits() < 25000 {}
+
+        // I honestly forgot why I print that twice...
+        ufmt::uwriteln!( & mut serial, "Hello, we are {} cms away from target!\r", value).void_unwrap();
+    }
+}
